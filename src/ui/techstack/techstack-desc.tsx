1d2d29761d89687ecce6d7e5e8a420bcdd17c76e
gelotuason <gelotuason@gmail.com>
2024-07-01 23:03:46 +0800

    add margin

--- src/ui/techstack/techstack-desc.tsx
+++ src/ui/techstack/techstack-desc.tsx
@@ -1,6 +1,6 @@
 export default function TechstackDescription() {
     return (
-        <div className="h-max border-l-4 ps-3 border-accent">
+        <div className="h-max border-l-4 ps-3 border-accent mt-6">
             <p>
                 My skills are a strategic blend of advanced tools designed to deliver <span className="text-accent">high-quality</span>, <span className="text-accent">scalable</span>, and <span className="text-accent">efficient solutions</span>.
                 It’s more than just a collection of technologies; it’s a competitive advantage that elevates your web development experience to new heights.

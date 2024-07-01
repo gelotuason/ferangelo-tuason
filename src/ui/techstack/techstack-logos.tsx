ebbedc7b7f5c7dbea2caf2af67dad851f9d0ddd2
gelotuason <gelotuason@gmail.com>
2024-07-01 23:02:19 +0800

    add logo

---
+++ src/ui/techstack/techstack-logos.tsx
@@ -0,0 +1,53 @@
+import bootstrap from '../../../public/bootstrap.png';
+import chakraUI from '../../../public/chakra-ui.png';
+import css from '../../../public/css.png';
+import firebase from '../../../public/firebase.png';
+import git from '../../../public/git.png';
+import github from '../../../public/github.png';
+import html from '../../../public/html.png';
+import javascript from '../../../public/js.png';
+import materialUI from '../../../public/mui.png';
+import next from '../../../public/next.png';
+import react from '../../../public/react.png';
+import shadcn from '../../../public/shadcn.png';
+import tailwindcss from '../../../public/tailwind-css.png';
+
+import Image from 'next/image';
+
+const imageProperties = [
+    { src: bootstrap, width: 30, height: 30 },
+    { src: chakraUI, width: 30, height: 30 },
+    { src: css, width: 30, height: 30 },
+    { src: firebase, width: 30, height: 30 },
+    { src: git, width: 30, height: 30 },
+    { src: github, width: 30, height: 30 },
+    { src: html, width: 30, height: 30 },
+    { src: javascript, width: 30, height: 30 },
+    { src: materialUI, width: 30, height: 30 },
+    { src: next, width: 30, height: 30 },
+    { src: react, width: 30, height: 30 },
+    { src: shadcn, width: 30, height: 30 },
+    { src: tailwindcss, width: 30, height: 30 },
+]
+
+
+export default function TechstackLogos() {
+    return (
+        <figure className='grid grid-cols-5 gap-4 mt-6 items-center'>
+            {
+                imageProperties.map((property, index) => {
+                    return (
+                        <Image
+                            key={index}
+                            src={property.src}
+                            width={50}
+                            height={50}
+                            alt='Tech Stack Logo'
+                            style={{ width: 'auto', height: 'auto' }}
+                        />
+                    )
+                })
+            }
+        </figure>
+    )
+}
--- src/ui/techstack/techstack-section.tsx
+++ src/ui/techstack/techstack-section.tsx
@@ -1,10 +1,15 @@
-import TechstackDescription from "./techstack-desc"
+import TechstackDescription from "./techstack-desc";
+import TechstackLogos from "./techstack-logos";
 
 export default function TechstackSection() {
     return (
         <section className="container h-max mt-28 mb-28">
             <h1 className="font-bold text-6xl">Tech Stack<span className="text-accent">.</span></h1>
             <TechstackDescription />
+            <div className="grid justify-center mt-10">
+                <h2 className="text-center">These are the tools I mostly used:</h2>
+                <TechstackLogos />
+            </div>
         </section>
     )
 }

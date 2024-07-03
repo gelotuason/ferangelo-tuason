513a3ddaad8fe6a283fd9ef645951dd1e02c5634
gelotuason <gelotuason@gmail.com>
2024-07-03 20:12:00 +0800

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Nav from "../ui/nav";
+import Footer from "@/ui/footer";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${montserrat.className}`}>
         <Nav />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/ui/footer.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const footerLinks = [
+    { name: 'Home', href: '/' },
+    { name: 'About', href: '#about' },
+    { name: 'Services', href: '#services' },
+    { name: 'Projects', href: '#projects' },
+    { name: 'Contact', href: '#contact' }
+]
+
+export default function Footer() {
+    return (
+        <footer className="border-t border-secondary py-4 h-max container relative flex flex-col items-center justify-center">
+            <FooterMenu />
+            <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight mt-4 mb-4">
+                Gelo<span className="text-accent">.</span>
+            </h2>
+            <p>© Ferangelo Tuason. All rights reserved.</p>
+        </footer>
+    )
+}
+
+function FooterMenu() {
+    const pathname = usePathname();
+
+    return (
+        <div>
+            {
+                footerLinks.map(link => (
+                    <Link
+                        key={link.name}
+                        href={link.href}
+                        className={clsx(
+                            'hover:text-accent px-4 py-2',
+                            {
+                                'text-accent': pathname === link.href
+                            }
+                        )}
+                    >
+                        {link.name}
+                    </Link>
+                ))
+            }
+        </div>
+    )
+}

2d8a3b70402b0028fb7d3ebc7769db45388efb9c
gelotuason <gelotuason@gmail.com>
2024-07-03 19:45:24 +0800

    change icon and refactor code

--- src/ui/contact/contact-tabs.tsx
+++ src/ui/contact/contact-tabs.tsx
@@ -1,39 +1,68 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { MobileIcon, EnvelopeClosedIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
-import { Email, Mobile, Linkedin, WhatsApp } from "./contact-list";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faLink } from "@fortawesome/free-solid-svg-icons";
+import { ContactLink } from "./contact-links";
+
+
+const email = <FontAwesomeIcon icon={faEnvelope} className="w-5 h-5" />;
+const linkedin = <FontAwesomeIcon icon={faLinkedinIn} className="w-5 h-5" />;
+const whatsapp = <FontAwesomeIcon icon={faWhatsapp} className="w-5 h-5" />;
+const link = <FontAwesomeIcon icon={faLink} className="w-5 h-5" />;
+
+const contactData = [
+    {
+        tabsTriggerText: 'Email',
+        tabsValue: 'email',
+        tabsTriggerIcon: email,
+        tabsContentText: 'gelotuason@gmail.com',
+        tabsContentIcon: link,
+        href: 'mailto:gelotuason@gmail.com'
+    },
+    {
+        tabsTriggerText: 'Linkedin',
+        tabsValue: 'linkedin',
+        tabsTriggerIcon: linkedin,
+        tabsContentText: 'Ferangelo Tuason',
+        tabsContentIcon: link,
+        href: 'https://www.linkedin.com/in/ferangelo-tuason/'
+    },
+    {
+        tabsTriggerText: 'WhatsApp',
+        tabsValue: 'whatsapp',
+        tabsTriggerIcon: whatsapp,
+        tabsContentText: '+63 936 702 7624',
+        tabsContentIcon: link,
+        href: 'https://wa.me/+639367027624'
+    },
+];
 
 export default function ContactTabs() {
     return (
-        <Tabs defaultValue="email" className="grid place-content-center">
-            <TabsList className="mb-2">
-                <TabsTrigger value="email">
-                    <EnvelopeClosedIcon />
-                    Email
-                </TabsTrigger>
-                <TabsTrigger value="mobile">
-                    <MobileIcon className="w-6 h-6" />
-                    Mobile
-                </TabsTrigger>
-                <TabsTrigger value="linkedin">
-                    <LinkedInLogoIcon />
-                    Linkedin
-                </TabsTrigger>
-                <TabsTrigger value="whatsapp">
-                    WhatsApp
-                </TabsTrigger>
-            </TabsList>
-            <TabsContent value="email">
-                <Email />
-            </TabsContent>
-            <TabsContent value="mobile">
-                <Mobile />
-            </TabsContent>
-            <TabsContent value="linkedin">
-                <Linkedin />
-            </TabsContent>
-            <TabsContent value="whatsapp">
-                <WhatsApp />
-            </TabsContent>
+        <Tabs defaultValue="email">
+            <>
+                <TabsList className="mb-2">
+                    {
+                        contactData.map(data => (
+                            <TabsTrigger value={data.tabsValue} key={data.tabsValue}>
+                                {data.tabsTriggerIcon}
+                                {data.tabsTriggerText}
+                            </TabsTrigger>
+                        ))
+                    }
+                </TabsList>
+                {
+                    contactData.map(data => (
+                        <TabsContent value={data.tabsValue} key={data.tabsValue}>
+                            <ContactLink
+                                href={data.href}
+                                icon={data.tabsContentIcon}
+                                text={data.tabsContentText}
+                            />
+                        </TabsContent>
+                    ))
+                }
+            </>
         </Tabs>
     )
 }

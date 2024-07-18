26fcc5ac826cdb46ce59e19e4ee58296938d070d
gelotuason <gelotuason@gmail.com>
2024-07-18 22:07:12 +0800

    change attribute and add height

--- src/app/ui/home/projects/projects-mockup.tsx
+++ src/app/ui/home/projects/projects-mockup.tsx
@@ -26,13 +26,12 @@ export default function ProjectsMockup() {
     return (
         projectMockupProperties.map(property => {
             return (
-                <div key={property.name} className="group relative">
+                <div key={property.name} className="group relative min-h-[320px] md:h-[480px]">
                     <Image
                         src={property.imageSrc}
-                        width={500}
-                        height={500}
+                        fill
                         alt="Project Mockup"
-                        className="object-cover rounded-xl w-full"
+                        className="object-cover rounded-xl"
                     />
                     <div className="absolute inset-0 text-primary bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 gap-2 flex flex-col justify-center items-center transition-opacity duration-150 rounded-xl">
                         <LiveURL liveUrl={property.liveUrl} />
